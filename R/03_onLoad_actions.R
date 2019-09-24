d075c6a40e2c6e796ca29ae649919715fe0333a0
Richard White <w@rwhite.no>
2019-09-24 11:30:42 +0300

    Fixing onLoad issue
    onLoad would make database calls. This is now corrected with a tryCatch statement.

--- R/03_onLoad_actions.R
+++ R/03_onLoad_actions.R
@@ -4,27 +4,32 @@
     load_package = FALSE
   )
 
-  actions[["sykdomspuls_obs"]] <- fd::action$new(
-    key = "ui_sykdomspuls_obs",
-    value = fhi::isoyearweek(sykdomspuls_date()),
-    dev_always_performs = TRUE,
-    production_days = c(3:5),
-    first_date_of_production = "2019-09-21"
-  )
+  tryCatch({
+    actions[["sykdomspuls_obs"]] <- fd::action$new(
+      key = "ui_sykdomspuls_obs",
+      value = fhi::isoyearweek(sykdomspuls_date()),
+      dev_always_performs = TRUE,
+      production_days = c(3:5),
+      first_date_of_production = "2019-09-21"
+    )
 
-  actions[["sykdomspuls_alert_pdf"]] <- fd::action$new(
-    key = "sykdomspuls_alert_pdf",
-    value = fhi::isoyearweek(sykdomspuls_date()),
-    dev_always_performs = TRUE,
-    production_days = c(3:5),
-    first_date_of_production = "2019-09-21"
-  )
+    actions[["sykdomspuls_alert_pdf"]] <- fd::action$new(
+      key = "sykdomspuls_alert_pdf",
+      value = fhi::isoyearweek(sykdomspuls_date()),
+      dev_always_performs = TRUE,
+      production_days = c(3:5),
+      first_date_of_production = "2019-09-21"
+    )
 
-  actions[["normomo_email"]] <- fd::action$new(
-    key = "normomo_email",
-    value = normomo_yrwk(),
-    dev_always_performs = TRUE,
-    production_days = c(2:3),
-    first_date_of_production = "2019-09-21"
-  )
+    actions[["normomo_email"]] <- fd::action$new(
+      key = "normomo_email",
+      value = normomo_yrwk(),
+      dev_always_performs = TRUE,
+      production_days = c(2:3),
+      first_date_of_production = "2019-09-21"
+    )
+  },
+  error=function(e){
+    fd::msg("No database available")
+  })
 }
