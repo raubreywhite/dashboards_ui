74fa9764836b881f1a814eab7e8af1133a7fb073
Richard White <w@rwhite.no>
2019-09-25 07:52:11 +0300

    Try with cairo

--- inst/src/RunProcess.R
+++ inst/src/RunProcess.R
@@ -1,7 +1,7 @@
 fd::initialize("ui")
 suppressMessages(library(data.table))
 suppressMessages(library(ggplot2))
-
+options(bitmapType = 'cairo', device = 'png')
 normomo$new()$run_all()
 
 sykdomspuls_alert_pdf$new()$run_all()
