c3c651ad85b0812f4d8d2d00933bfe54521caf3f
Travis CI User <travis@example.org>
2020-03-03 15:19:06 +0000

    Travis CI update 123 [ci skip]

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: ui")
-  packageStartupMessage("Version 2020.02.19 at 13:48")
+  packageStartupMessage("Version 2020.03.03 at 15:09")
   packageStartupMessage(glue::glue("Developed by Richard White, Gunnar R{fhi::nb$oe}"))
   packageStartupMessage("Norwegian Institute of Public Health")
 }
